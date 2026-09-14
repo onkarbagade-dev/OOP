083cbb892b6de219ede0afd23841af775882abc3
𝙾𝚗𝚔𝚊𝚛 𝙱𝚊𝚐𝚊𝚍𝚎 <onkarbagade4508@gmail.com>
2026-09-14 10:08:15 +0530

    Change student class members to public access

--- student.cpp
+++ student.cpp
@@ -2,12 +2,43 @@
 #include <string>
 using namespace std;
 
-class student 
-{ 
-  private:
-         int studentID;
-         int year;
-         string branch;
-         string studentName;
+class student
+{
+public:
+    int studentID;
+    int year;
+    string branch;
+    string studentName;
 
-void input
+    void input()
+    {
+        cout << "Enter studentID: ";
+        cin >> studentID;
+
+        cout << "year: ";
+        cin >> year;
+
+        cout << "branch: ";
+        cin >> branch;
+
+        cout << "studentName: ";
+        cin >> studentName;
+    }
+
+    void display()
+    {
+        cout << "\n--- Student Details ---" << endl;
+        cout << "student ID: " << studentID << endl;
+        cout << "year: " << year << endl;
+        cout << "branch: " << branch << endl;
+        cout << "studentName: " << studentName << endl;
+    }
+};
+
+int main()
+{
+    student s;
+    s.input();
+    s.display();
+    return 0;
+}
