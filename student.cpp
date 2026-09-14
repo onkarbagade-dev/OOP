115c9e3476adc1554d956a9f859a2c277b8e1cf8
𝙾𝚗𝚔𝚊𝚛 𝙱𝚊𝚐𝚊𝚍𝚎 <onkarbagade4508@gmail.com>
2026-09-14 09:40:07 +0530

    Add student class with private attributes

---
+++ student.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class student 
+{ 
+  private:
+         int studentID;
+         int year;
+         string branch;
+         string studentName;
+
+void input
